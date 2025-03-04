73397d345557a7b27f1182df03058c5dcbc93997
Fidel Yong <fidelyong22@gmail.com>
2025-03-04 11:16:31 +0800

    Fix the homepage not showing on entry

--- src/App.jsx
+++ src/App.jsx
@@ -5,21 +5,22 @@ import AboutPage from "./pages/AboutPage.jsx";
 import Contacts from "./pages/Contacts.jsx";
 import Footer from "./components/Footer.jsx";
 import Navbar from "./components/Navbar.jsx";
-import './App.css';
+import "./App.css";
 
 const App = () => {
   return (
     <Router>
       <div className="App">
         <Navbar />
-          <div className="content">
-            <Routes>
-              <Route path="/pages/HomePage" element={<HomePage />}></Route>
-              <Route path="/pages/Projects" element={<Projects />}></Route>
-              <Route path="/pages/AboutPage" element={<AboutPage />}></Route>
-              <Route path="/pages/Contacts" element={<Contacts />}></Route>
-            </Routes>
-          </div>
+        <div className="content">
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/pages/HomePage" element={<HomePage />} />
+            <Route path="/pages/Projects" element={<Projects />}></Route>
+            <Route path="/pages/AboutPage" element={<AboutPage />}></Route>
+            <Route path="/pages/Contacts" element={<Contacts />}></Route>
+          </Routes>
+        </div>
         <Footer />
       </div>
     </Router>
