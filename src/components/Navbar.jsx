9b1cf509d731ca92747eb6f5c46db7ef45a20b5f
Fidel Yong <fidelyong22@gmail.com>
2025-03-27 21:29:33 +0800

    Add responsiveness to navbar and home
    Edit Navbar and Home to be mobile responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,40 @@
+import { useState } from "react";
+import { FiMenu, FiX } from "react-icons/fi"; // Import hamburger and close icons
 import "./Navbar.css";
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
-    <nav className="navbar flex items-center justify-between text-md font-poppins font-bold p-6 sticky top-0 bg-white z-50">
+    <nav className="navbar flex items-center justify-between text-md font-poppins font-bold p-6 sticky top-0 bg-white backdrop-blur-md z-50">
       <span>Fidel.dev</span>
-      <div className="flex space-x-6">
-        <a href="#home" className="hover:scale-105">Home</a>
-        <a href="#about" className="hover:scale-105">About</a>
-        <a href="#projects" className="hover:scale-105">Projects</a>
-        <a href="#contacts" className="hover:scale-105">Contact</a>
+
+      {/* Desktop Nav */}
+      <div className="hidden sm:flex space-x-6">
+        <a href="#home" className="hover:scale-105 transition-transform">Home</a>
+        <a href="#about" className="hover:scale-105 transition-transform">About</a>
+        <a href="#projects" className="hover:scale-105 transition-transform">Projects</a>
+        <a href="#contacts" className="hover:scale-105 transition-transform">Contact</a>
       </div>
+
+      {/* Mobile Menu Button */}
+      <button className="sm:hidden" onClick={() => setIsOpen(!isOpen)}>
+        {isOpen ? <FiX className="w-6 h-6" /> : <FiMenu className="w-6 h-6" />}
+      </button>
+
+      {/* Mobile Menu */}
+      {isOpen && (
+        <div className="grid grid-cols-1 absolute top-14 left-0 w-screen h-screen bg-white/90 sm:hidden" onClick={() => setIsOpen(!isOpen)}>
+          <div></div>
+          <a href="#home" className="hover:scale-105 transition-transform">Home</a>
+          <a href="#about" className="hover:scale-105 transition-transform">About</a>
+          <a href="#projects" className="hover:scale-105 transition-transform">Projects</a>
+          <a href="#contacts" className="hover:scale-105 transition-transform">Contact</a>
+          <div></div>
+          <div></div>
+          
+        </div>
+      )}
     </nav>
   );
 };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,21 +7,22 @@ import {
   FaBootstrap,
   FaReact,
 } from "react-icons/fa";
+import { SiTailwindcss } from "react-icons/si";
 import { FaGithub, FaLinkedin } from "react-icons/fa";
 import image from "../assets/Resume-poto.jpg";
 
 const Home = () => {
   return (
-    <div className="flex flex-col items-center h-screen">
-      <div className="font-poppins mt-28 lg:w-1/2 w-full">
+    <div className="flex flex-col items-center h-screen px-4 md:px-8 lg:px-16">
+      <div className="font-poppins mt-28 md:w-2/3 xl:w-1/2 w-full">
         <div className="flex justify-center">
           <div className="flex flex-col text-left">
-            <div className=" font-bold text-xl md:text-2xl lg:text-6xl">
+            <div className=" font-bold text-xl lg:text-5xl">
               <p>Front-End React</p>
               <p>Developer 👋</p>
             </div>
 
-            <div className="text-sm text-[#bcbabb] mt-4">
+            <div className="text-xs text-[#bcbabb] mt-4">
               <p>Hi, I&apos;m Fidel Yong. A passionate Front-end React</p>
               <p>Developer based in Malaysia, Sabah.</p>
             </div>
@@ -36,25 +37,27 @@ const Home = () => {
             </div>
           </div>
 
-          <div>
-            <img
-              className="ml-4 h-full w-full md:ml-7 md:ml-10 md:h-32 md:w-32 lg:h-64 lg:w-64 rounded-full"
-              src={image}
-              alt=""
-            />
+          <div className="ml-7 ml-10 min-w-32 w-32 md:w-48 lg:w-64 ">
+            <img className="rounded-full" src={image} alt="" />
           </div>
         </div>
       </div>
 
-      <div className="flex mt-4 mb-40 space-x-4 md:space-x-8 lg:space-x-14 lg:w-1/2 w-full text-left">
-        <span className="font-bold mr-10">Tech Stack</span>
-        <div className="border-l border-gray-400 h-6 pr-35"></div>
-        <FaHtml5 className="w-8 h-8 text-[#e34c26]" />
-        <FaCss3Alt className="w-8 h-8 text-[#264de4]" />
-        <FaPhp className="w-8 h-8 text-[#777bb4]" />
-        <FaDatabase className="w-8 h-8 text-[#00758f]" />
-        <FaBootstrap className="w-8 h-8 text-[#7952b3]" />
-        <FaReact className="w-8 h-8 text-[#61dafb]" />
+      <div className="flex items-center justify-center gap-5 mt-10 mb-40 lg:w-1/2 w-full text-left">
+        <div className="font-bold text-sm">
+          <p>Tech</p>
+          <p>Stack</p>
+        </div>
+        <div className="border-l border-gray-400 h-14"></div>
+        <div className="grid grid-cols-7 gap-4 text-xs">
+          <FaHtml5 className="w-6 h-6 text-[#e34c26] hover:shadow hover:rounded" />
+          <FaCss3Alt className="w-6 h-6 text-[#264de4] hover:shadow hover:rounded" /> 
+          <FaBootstrap className="w-6 h-6 text-[#7952b3] hover:shadow hover:rounded" />
+          <FaReact className="w-6 h-6 text-[#61dafb]" />
+          <SiTailwindcss className="w-6 h-6 text-[#38BDF8] hover:shadow hover:rounded" />
+          <FaDatabase className="w-6 h-6 text-[#00758f] hover:shadow hover:rounded" />
+          <FaPhp className="rounded w-6 h-6 text-[#777bb4] hover:shadow hover:rounded" />
+        </div>
       </div>
     </div>
   );
